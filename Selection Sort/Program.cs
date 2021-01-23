fcf87ce777471197c73cd48a4afaa7418aab9604
Serdar Sarı <iserdarsari@gmail.com>
2021-01-23 13:53:37 +0300

    Update Program.cs

--- Selection Sort/Program.cs
+++ Selection Sort/Program.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace SelectionSort
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // SELECTION SORT
 
             int[] dizi = { 2, 3, 1, 8, -6,-9 };
             int temp=0;
